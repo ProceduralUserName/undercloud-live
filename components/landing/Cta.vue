301ac745a83b8c0fa615c29138ca7bdc01119e3c
ProceduralUserName <simonbrubacher@gmail.com>
2024-11-23 21:28:43 -0500

    Homepage - completed
    The Homepage is ready to enter QA

--- components/landing/Cta.vue
+++ components/landing/Cta.vue
@@ -2,12 +2,20 @@
   <div
     class="bg-black px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center"
   >
-    <h2 class="text-white text-3xl md:text-6xl">Build faster websites.</h2>
+    <h2 class="text-white text-3xl md:text-6xl">
+      Ready to strengthen that handshake?
+    </h2>
     <p class="text-slate-500 mt-4 text-lg md:text-xl">
-      Static or Dynamic, the choice is yours with Nuxt's hybrid rendering modes.
+      Let’s create a custom, hand-coded website that helps your business grow.
+      Contact us today for a free consultation and see how we can help you
+      succeed online.
     </p>
     <div class="flex mt-5">
-      <LandingLink href="#" styleName="inverted">Get Started</LandingLink>
+      <NuxtLink
+        class="router-link-active router-link-exact-active rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-5 py-2.5 bg-white text-black border-2 border-transparent"
+        to="contact"
+        >Get started</NuxtLink
+      >
     </div>
   </div>
 </template>
--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -3,12 +3,12 @@
     <div class="p-24 md:order-1 hidden md:block">
       <img
         class="rounded-full transition-transform transform hover:scale-125"
-        src="~/assets/img/hero.jpg"
+        src="~/assets/img/logo-color.png"
         alt="Starship starts the engine"
         loading="eager"
         format="avif"
-        width="512"
-        height="512"
+        width="1012"
+        height="1012"
       />
     </div>
 
@@ -24,20 +24,17 @@
         customers. Empower your business with a powerful online presence.
       </p>
       <div class="mt-6 flex flex-col sm:flex-row gap-3">
-        <LandingLink
-          href="https://github.com/Gr33nW33n/nuxtship-template"
-          target="_blank"
-          rel="noopener"
-          >Get Started</LandingLink
+        <NuxtLink
+          class="rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-5 py-2.5 bg-black text-white hover:bg-gray-800 border-2 border-transparent"
+          to="contact"
+          >Get Started</NuxtLink
         >
-        <LandingLink
-          size="lg"
-          styleName="outline"
-          rel="noopener"
-          href="https://github.com/Gr33nW33n/nuxtship-template"
-          target="_blank"
-          >View Repo</LandingLink
+        <NuxtLink
+          class="rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-5 py-2.5 bg-white border-2 border-black hover:bg-gray-100 text-black"
+          to="about"
         >
+          Why Us?
+        </NuxtLink>
       </div>
     </div>
   </main>
