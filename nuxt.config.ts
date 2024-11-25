53b00396a361f78ccae8da245e707c290bca6647
ProceduralUserName <simonbrubacher@gmail.com>
2024-11-25 10:06:48 -0500

    changed favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-
+  // app: {
+  //   head: {
+  //     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  //     title: "UnderCloud",
+  //   },
+  // },
   postcss: {
     plugins: {
       tailwindcss: {},
